2d6ec92fa33682c165267cf61dc1d4adafc8250d
olugboyegaonimole <39015008+olugboyegaonimole@users.noreply.github.com>
2018-08-20 03:59:56 +0100

    Add files via upload

---
+++ restfulAPIs.py
@@ -0,0 +1,199 @@
+
+# RESTful APIs
+
+
+# WHAT IS AN API?
+# MISCELLANEOUS API STUFF
+# WHICH OF THE FOLLOWING ARE TRUE?
+# WHAT ARE THE CONTENTS OF AN API?
+# WHAT IS THE FUNCTION OF A WEB API?
+# WHAT WORK ENVIRONMENT DO YOU NEED FOR WORKING WITH APIs?
+# WHAT IS REST?
+# WHAT IS A WEB SERVICE?
+# WHY GET DATA FROM A DEVICE'S REST API (USING PYTHON)?
+# WHAT IS POSTMAN?
+
+
+
+
+
+# WHAT IS AN API?
+
+
+
+
+# SOMETHING THAT ALLOWS ONE PIECE OF SOFTWARE TO TALK TO ANOTHER
+
+
+
+
+# a set of definitions, protocols and tools for building software
+
+
+# a method of communication between software components
+
+
+# a source of software building blocks 
+# the building blocks are then put together by the programmer
+
+
+
+
+
+
+
+
+
+# MISCELLANEOUS API STUFF
+
+
+# a REST API works similarly to a website
+
+	# - you make a call to a server 
+	# - you get data back via the HTTP protocol
+
+# there are similarities between a REST API call and loading up a web page
+
+	# eg make an API request to Facebook's graph API
+	# GET THE RESPONSE IN YOUR BROWSER
+	# THE RESPONSE YOU GET IS JSON-FORMATTED DATA
+
+# JSON javascript objject notation
+
+	# JSON data is organised in key-value pairs
+	# lightweight data-interchange format.
+	# easy for humans to read and write. 
+	# easy for machines to parse and generate.
+
+
+# an API allows me to connect my app to the server of a web-based service
+	
+	# by coding the services into my app
+	# by coding the API into my app
+	# i can use the company's services in my app
+	# i can use their infrastructure and bring more services to my users
+	# an API opens an interface that i can connect to via my app
+	# i can have an API on my app's back-end server
+	# eg if i use a server for my content and user management
+
+
+
+
+
+
+
+
+# WHICH OF THE FOLLOWING ARE TRUE?
+
+# the API makes the call?
+# the API receives the call?
+# the call passes through the API?
+# the call uses API rules or API language?
+
+
+
+
+
+
+
+
+# WHAT ARE SOME CONTENTS OF AN API?
+
+
+# specifications for routines
+
+# data structures
+
+# object classes 
+
+# variables 
+
+# remote calls
+
+
+
+
+
+
+
+# WHAT IS THE FUNCTION OF A WEB API?
+
+
+# to give out their data
+
+# to allow your web application communicate with that data
+
+# to allow you make new data
+
+
+
+
+
+
+# WHAT WORK ENVIRONMENT DO YOU NEED FOR WORKING WITH APIs?
+
+
+# Windows, linux or mac
+
+# Python
+
+# A code editor
+
+# A REST client eg Postman
+
+
+
+
+
+# WHAT IS REST?
+
+
+# An architectural style 
+
+# specifies constraints for creating web services
+
+# REpresentational State Transfer
+
+
+
+
+
+
+
+# WHAT IS A WEB SERVICE?
+
+
+# A method of communication between two devices over a network
+
+
+
+
+
+
+# WHY GET DATA FROM A DEVICE'S REST API (USING PYTHON)?
+
+# there is an explosion of IP connected devices 
+
+# networks are scaling ever larger
+
+# becoming more cumbersome to manage networks using CLI commands
+
+# easier to manage networks using code/programmatic approach
+
+
+
+
+
+
+
+# WHaT IS POSTMAN?
+
+
+# REST client
+
+# makes it easier to interact with and interrogate a device's REST API
+
+# previously available as extension to google chrome browser
+
+# postman interceptor comes pre-installed
+
