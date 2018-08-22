d6dfebaf9613a236eb238afeee8dde25c294f548
olugboyegaonimole <39015008+olugboyegaonimole@users.noreply.github.com>
2018-08-22 23:44:28 +0100

    Update flaskrestfulAPI v2.py

--- flaskrestfulAPI v2.py
+++ flaskrestfulAPI v2.py
@@ -1,1 +1,33 @@
-#flask
+# what is flask?
+
+# a micro framework for building web applications
+
+  # basic frame work
+
+  # simple set of components
+
+  # elegance and simplicity
+  
+# basic 'hello world' flask application:
+
+
+from flask import Flask
+
+
+
+#create Flask object
+app = Flask(__name__)
+
+
+
+
+#create route
+@app.route('/')
+def hello():
+  return "hello world"
+
+
+
+#run in debug mode
+if __name__=='__main__':
+  app.run(debug=True)
