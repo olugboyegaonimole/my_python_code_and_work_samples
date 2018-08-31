607a2f9b733dd72704997aa425767dcdda2da340
olugboyegaonimole <39015008+olugboyegaonimole@users.noreply.github.com>
2018-08-31 22:19:10 +0100

    Add files via upload

---
+++ APIs_personal_notes.py
@@ -0,0 +1,264 @@
+# API END POINT
+
+
+# GET DATA FROM APIs
+# WRITE DATA TO APIs
+
+
+
+
+# GET - GET DATA
+# POST - WRITE DATA
+
+# Best practice (WHEN WRITING DATA?) is to put the data in the body of the request
+
+
+
+
+# RESTful APIs FOR WEB APPLICATIONS
+
+
+# WHAT IS AN API?
+# MISCELLANEOUS API STUFF
+# WHICH OF THE FOLLOWING ARE TRUE?
+# EXAMPLES OF API  CALLS
+# WHAT IS THE FUNCTION OF A WEB API?
+# WHAT WORK ENVIRONMENT DO YOU NEED FOR WORKING WITH RESTful? APIs?
+# WHAT IS REST?
+# WHAT IS A WEB SERVICE?
+# WHY GET DATA FROM A DEVICE'S REST API (USING PYTHON)?
+# WHAT IS POSTMAN?
+
+
+
+
+
+# WHAT IS AN API?
+
+
+
+
+# A PROGRAM THAT ALLOWS ONE PIECE OF SOFTWARE TO TALK TO ANOTHER
+
+
+# AN API IS A PROGRAM ON A SERVER THAT ALLOWS ME TO 
+	
+	#CONNECT MY WEB APP TO THE SERVER AND 
+	#REPLICATE THE SERVER'S SERVICES ON MY WEB APP
+	#THEN MY USERS CAN USE THE ITS SERVICES FROM MY APP
+
+
+
+# AN API ALLOWS ME TO 
+
+	# integrate MY program with A WEBSITE and 
+	
+	# allow MY PROGRAM to perform the operations available on the website
+
+
+
+
+# a set of definitions, protocols and tools for building software
+
+
+
+# a set of methods of communication between software components
+
+
+
+# a source of software building blocks 
+# the building blocks are put together by a programmer
+
+
+
+# APPLICATION - A MACHINE
+# PROGRAMMING - GIVING INSTRUCTIONS
+# INTERFACE - A PROGRAM THAT A MACHINE USES FOR TAKING INPUT, AND GIVING OUTPUT
+
+
+# E.G. TWITTER HAS AN INTERFACE THAT HUMANS AND PROGRAMS CAN TALK TO
+
+
+# an API opens an interface on it's host that i can connect to
+
+# an API allows you create machines that talk to other machines
+
+# why would one machine want to talk to the next?
+
+	# so that it can use the services of the next
+
+# the API of one machine says to another machine "this is how you talk to us"
+
+# speaking to the API requires using specific syntax, formats
+
+# e.g. you can write the following API request into your browser address field and click on send http://graph.facebook.com/derekdahmer?fields=first_name
+
+
+
+
+
+
+
+
+# MISCELLANEOUS API STUFF
+
+
+# a REST API works similarly to a website
+
+	# - you make a call to a server 
+	# - you get data back via the HTTP protocol
+
+# there are similarities between a REST API call and loading up a web page
+
+	# eg make an API request to Facebook's graph API
+	# GET THE RESPONSE IN YOUR BROWSER
+	# THE RESPONSE YOU GET IS JSON-FORMATTED DATA
+
+# JSON javascript objject notation
+
+	# JSON data is organised in key-value pairs
+	# lightweight data-interchange format.
+	# easy for humans to read and write. 
+	# easy for machines to parse and generate.
+	
+
+
+# an API allows me to connect my app to the server of a web-based service
+	
+	# i code the services into my app
+	# i code the API into my app
+	
+	# I CAN USE THE COMPANY'S SERVICES IN MY APP
+
+	# i can use their infrastructure and bring more services to my users
+	# i can boost my app's functionality
+
+
+# an API opens an interface on it's host that i can connect to
+
+
+# i can equally have an API on my app's back-end server
+
+	# eg if i use a server for my content and user management
+
+	# other apps can connect to my app via my API
+
+
+
+
+
+
+
+
+# WHICH OF THE FOLLOWING ARE TRUE?
+
+# the API makes the call?
+# the API receives the call?
+# the call passes through the API?
+# the call uses API rules or API language?
+
+
+
+
+
+# EXAMPLES OF API CALLS
+
+# complete response to an API call/request e.g. by loading up http://graph.facebook.com/derekdahmer
+
+# only selected parameters e.g. by loading up  http://graph.facebook.com/derekdahmer?fields=id,name,likes
+
+# http://maps.googleapis.com/maps/api/geocode/json?address=lagos&sensor=false
+
+# SERVER maps.googleapis.com
+# RESOURCES /maps/api/geocode/json
+
+
+
+
+
+
+# WHAT IS THE FUNCTION OF A WEB API?
+
+
+# to give out their data
+
+# to allow your web application communicate with that data
+
+# to allow you make new data
+
+
+
+
+
+
+# WHAT WORK ENVIRONMENT DO YOU NEED FOR WORKING WITH RESTful? APIs?
+
+
+# AN OPERATING SYSTEM - Windows, linux or mac
+
+# A SCRIPTING LANGUAGE? - Python
+
+# A code editor
+
+# A REST client eg Postman
+
+
+
+
+
+# WHAT IS REST?
+
+
+# An architectural style 
+
+# specifies constraints for creating web services
+
+	# ie specifies constraints for communicating over a network
+
+# REpresentational State Transfer
+
+
+
+
+
+
+
+# WHAT IS A WEB SERVICE?
+
+
+# A method of communication 
+# between two devices 
+# over a network
+
+
+
+
+
+
+# WHY GET DATA FROM A DEVICE'S REST API (USING PYTHON)?
+
+# today there is an explosion of IP connected devices 
+
+# networks are scaling ever larger
+
+# becoming more cumbersome to manage networks using CLI commands
+
+# easier to manage networks using code (a programmatic approach)
+
+
+
+
+
+
+
+# WHAT IS POSTMAN?
+
+
+# A REST client
+
+# makes it easier to interact with and interrogate a device's REST API
+
+# previously available as extension to google Chrome browser
+
+# postman interceptor now comes pre-installed with Chrome browser?
+
---
+++ vanilla_flask_API_example2.py
@@ -0,0 +1,125 @@
+# BUILD A REST API USING PYTHON AND VANILLA FLASK
+
+
+
+# INTRODUCTION: BUILD A BASIC FLASK WEB APPLICATION
+
+# MAIN TASK: BUILD A REST API USING PYTHON AND VANILLA FLASK
+
+
+
+# what is flask?
+
+# a micro framework for building web applications
+
+  # very basic framework
+
+  # simple set of components - can be extended 
+
+  # elegance and simplicity
+
+  # comes with several extensions eg RESTful?
+  
+
+
+
+
+# INTRODUCTION: BUILD A BASIC FLASK WEB APPLICATION
+
+
+from flask import Flask
+
+
+#create Flask object
+app = Flask(__name__)
+
+
+#create URI ENDPOINT?
+@app.route('/')
+def hello():
+  return "hello TESTING"
+
+
+#run in debug mode
+if __name__=='__main__':
+  app.run(debug=True, port=8080)
+
+
+
+
+
+
+
+# MAIN TASK: BUILD A REST API USING PYTHON AND VANILLA FLASK
+
+#EXAMPLE 1
+
+from flask import Flask, jsonify
+
+
+#create Flask object
+app = Flask(__name__)
+
+
+#create URI ENDPOINT
+@app.route('/') # decorating the function hello() with this route converts the function to a URI endpoint
+def hello():
+  return jsonify({'message':"hello TESTING"}) # the json format converts the web app to a REST API
+
+
+#run in debug mode
+if __name__=='__main__':
+  app.run(debug=True, port=8080)
+
+
+
+
+
+#EXAMPLE 2
+
+from flask import Flask, jsonify, request
+
+
+# here you can
+
+	#have more routes
+	#have more parameters as input to the URI
+	#have other (works?) HTTP methods
+
+#response codes
+
+	#default for a GET request is 200
+
+
+
+app =  Flask(__name__)
+
+
+@app.route('/multiply/<int:number>', methods=['GET', 'POST']) # this endpoint accepts values that you can process further
+def myfunction(number):
+	return jsonify({'result':number * 89})
+
+
+@app.route('/', methods=['GET', 'POST'])
+def myfunction2():
+
+	#check the request type
+	if request.method=='POST':
+
+		#extract the content of the request
+		message = request.get_json()
+
+		#send the content back in the response
+		return jsonify({'you sent this':message}), 201
+
+	else:
+		return jsonify({'message':'this is a GET request'})
+
+
+if __name__=='__main__':
+	app.run(debug=True, port=8080)
+
+
+
+
+
